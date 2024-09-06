b779ab3e73d6175c72263467fb0fdd563bd92332
Chad Ferguson <csfergu@gmail.com>
2024-09-06 13:23:54 -0500

    init2

---
+++ Components/Pages/EmployeeDetail.razor.cs
@@ -0,0 +1,22 @@
+﻿using BethanysPieShopHRM.Services;
+using BethanysPieShopHRM.Shared.Domain;
+using Microsoft.AspNetCore.Components;
+
+namespace BethanysPieShopHRM.Components.Pages;
+
+public partial class EmployeeDetail
+{
+    [Parameter] public int EmployeeId { get; set; }
+
+    public Employee Employee { get; set; } = new();
+
+    protected override void OnInitialized()
+    {
+        Employee = MockDataService.Employees!.Single(e => e.EmployeeId == EmployeeId);
+    }
+
+    private void ChangeHolidayState()
+    {
+        Employee.IsOnHoliday = !Employee.IsOnHoliday;
+    }
+}
---
+++ Components/Pages/EmployeeOverview.razor.cs
@@ -0,0 +1,23 @@
+﻿using BethanysPieShopHRM.Services;
+using BethanysPieShopHRM.Shared.Domain;
+
+namespace BethanysPieShopHRM.Components.Pages;
+
+public partial class EmployeeOverview
+{
+    private Employee? _selectedEmployee;
+    public List<Employee>? Employees { get; set; }
+
+    private string Title = "Employee overview";
+
+    protected override async Task OnInitializedAsync()
+    {
+        await Task.Delay(2000);
+        Employees = MockDataService.Employees!;
+    }
+
+    public void ShowQuickViewPopup(Employee selectedEmployee)
+    {
+        _selectedEmployee = selectedEmployee;
+    }
+}
